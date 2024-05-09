d442f68234a898a147dcdb390814302d5b609551
Nimbus6001 <146570180+Nimbus6001@users.noreply.github.com>
2024-05-09 12:57:22 +0200

    added onEnable and onDisable info messages

--- kektorial/src/main/java/org/nimbus/kektorial/Kektorial.java
+++ kektorial/src/main/java/org/nimbus/kektorial/Kektorial.java
@@ -7,11 +7,13 @@ public final class Kektorial extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("Kektorial running");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("Kektorial stopped");
+
     }
 }
