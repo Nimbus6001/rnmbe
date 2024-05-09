f683917f14e7a3ba2fec722c69d86ab41b422ac0
Nimbus6001 <146570180+Nimbus6001@users.noreply.github.com>
2024-05-09 15:49:25 +0200

    added config.yml

--- kektorial/src/main/java/org/nimbus/kektorial/Kektorial.java
+++ kektorial/src/main/java/org/nimbus/kektorial/Kektorial.java
@@ -1,14 +1,19 @@
 package org.nimbus.kektorial;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.nimbus.kektorial.commands.Info;
 import org.nimbus.kektorial.welcome.WelcomeEvents;
 
 public final class Kektorial extends JavaPlugin {
 
+    public static FileConfiguration config:
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        saveDefaultConfig();
+        config = getConfig();
         getCommand("info").setExecutor(new Info());
 
         getServer().getPluginManager().registerEvents(new WelcomeEvents(), this);
--- kektorial/src/main/java/org/nimbus/kektorial/welcome/WelcomeEvents.java
+++ kektorial/src/main/java/org/nimbus/kektorial/welcome/WelcomeEvents.java
@@ -4,12 +4,13 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.nimbus.kektorial.Kektorial;
 
 public class WelcomeEvents implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         if (event.getPlayer().hasPermission("join.silent"))
             event.setJoinMessage("");
-        event.getPlayer().sendMessage(ChatColor.GOLD + "Willkommen, " + event.getPlayer().getName() + ". Du Kek.");
+        event.getPlayer().sendMessage(ChatColor.GOLD + "Willkommen, " + event.getPlayer().getName() + ". Du " + Kektorial.config.getString("onPlayerJoin.slur") + ".";
     }
 }
