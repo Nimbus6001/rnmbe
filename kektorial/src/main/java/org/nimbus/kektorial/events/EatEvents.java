f891072a713484aaf932e17dc4f4dda31de1e793
Nimbus6001 <146570180+Nimbus6001@users.noreply.github.com>
2024-05-10 16:26:27 +0200

    before CodingSession

--- kektorial/src/main/java/org/nimbus/kektorial/events/EatEvents.java
+++ kektorial/src/main/java/org/nimbus/kektorial/events/EatEvents.java
@@ -4,6 +4,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 
+import java.util.Objects;
+
 public class EatEvents implements Listener {
 
     @EventHandler
